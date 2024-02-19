0e434c7bc3fef148c5c06d12606310d734bf43b1
R1CONE <116386177+R1CONE@users.noreply.github.com>
2024-02-19 14:14:30 +0100

    Create recjump_data_baze.sql

---
+++ Recoil jump/test.loc/recjump_data_baze.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 19, 2024 at 02:11 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `recjump_data_baze`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `nickname` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`nickname`, `email`, `password`, `id`) VALUES
+('r1cone', 'mikhail.siruts@gmail.com', '123123123', 1),
+('qweqweqwe', 'damir.sydorchuk@gmail.com', '213241', 2),
+('dfgdfg', 'gggggil.siruts@gmail.com', '1yercvdsf3', 3),
+('gfdrefgd', 'sergiusz.salata@gmail.com', '1234sd12', 4);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
